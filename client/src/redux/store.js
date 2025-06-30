4161dda16f59c7fa7113fbe339bb07df8c494f32
Saikrishna2k05 <spasupul11@gitam.in>
2025-07-01 00:48:28 +0530

    Redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './authSlice.js'
 import blogReducer from './blogSlice.js';
-const store=configureStore({
-    reducer:{
-        auth: authReducer,
-        blog:blogReducer
-    }
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+const persistConfig={
+    key: 'root',
+    storage,
+    whitelist: ['auth']
+}
+const rootReducer=combineReducers({
+    auth: authReducer,
+    blog:blogReducer
 })
+const persistedReducer=persistReducer(persistConfig, rootReducer);
+const store=configureStore({
+    reducer: persistedReducer,
+})
+export const persistor = persistStore(store);
 export default store
